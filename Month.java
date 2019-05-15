389a1d8d1da413faa0df6099741b32d5b72a9e71
Kakimzhanova Kyz-Zhibek <z.kakimzhanova@mail.ru>
2019-05-15 22:25:36 +0600

    Optional Task 3

---
+++ Month.java
@@ -0,0 +1,35 @@
+package opt3;
+import java.util.Scanner;
+public class Month{
+	private static String[] monthArray = {	"January", 
+							"February", 
+							"March", 
+							"April", 
+							"May", 
+							"June", 
+							"July", 
+							"August", 
+							"September", 
+							"October", 
+							"November", 
+							"December"
+						};
+	public void printMonthName(int i){
+		System.out.println(monthArray[i-1]);
+	}
+
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter month number:");
+		int monthNumber = scan.nextInt();
+
+		while ((monthNumber <= 0)||(monthNumber >= 13)){
+			System.out.println("Enter integer number from 1 to 12");
+			monthNumber = scan.nextInt();
+		}
+		scan.close();
+
+		Month m = new Month();
+		m.printMonthName(monthNumber);
+	}
+}
